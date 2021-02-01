d16fade67d7c8a997a5a89695912402e18fd6c64
Wdy2433438568 <49870921+Wdy2433438568@users.noreply.github.com>
2021-02-01 11:48:12 +0000

    Create 打开文件类.py

---
+++ 打开文件类.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import easygui as g
+class Openfile:
+    def __init__(self):
+        g.msgbox('我启动啦！','启动')
+    
+    def openfile(self):
+        with open(g.fileopenbox(default='*.txt')) as f1:
+            a = f1.read()
+            g.textbox(text=a,msg='文件内容如下',title='')
+            f1.close() 
+    
+    def __del__(self):
+        g.msgbox('我关闭啦！','关闭')
+
+
+opf = Openfile()
+opf.openfile()
